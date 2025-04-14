784b86cdebc728d6cc7690c98989aee0a742d299
tamim1one <oneclassnetfan@gmail.com>
2025-04-14 12:42:48 +0600

    updated inputfields desing and functionalties.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
         resizeToAvoidBottomInset: true, // Prevent overflow when keyboard opens
@@ -20,7 +21,6 @@ class MyApp extends StatelessWidget {
               child: SingleChildScrollView(
                 padding: EdgeInsets.symmetric(
                   horizontal: screenWidth * 0.05,
-
                 ), // Responsive padding
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -31,43 +31,58 @@ class MyApp extends StatelessWidget {
                       'assets/image.jpg',
                       height: screenHeight * 0.4, // Responsive height
                     ),
-                    SizedBox(height: screenHeight * 0.03), // Responsive spacing
+                    SizedBox(height: screenHeight * 0.01), // Responsive spacing
                     // Login title
                     Align(
                       alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          fontSize: screenWidth * 0.07, // Responsive font size
-                          fontWeight: FontWeight.bold,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 27, 55, 77),
+                            fontSize: screenWidth * 0.1, // Responsive font size
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                        textAlign: TextAlign.center,
                       ),
                     ),
-                    SizedBox(height: screenHeight * 0.01),
+                    SizedBox(height: screenHeight * 0.001),
 
                     // Subtitle
                     Align(
                       alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Please Sign in to continue.',
-                        style: TextStyle(
-                          fontSize: screenWidth * 0.04, // Responsive font size
-                          color: Colors.grey,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: Text(
+                          'Please Sign in to continue.',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: screenWidth * 0.05, // Responsive font size
+                            color: Color.fromARGB(255, 27, 55, 77),
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                        textAlign: TextAlign.center,
                       ),
                     ),
                     SizedBox(height: screenHeight * 0.04),
 
                     // Username field
                     TextField(
+                      obscureText: false,
                       decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.person),
-                        hintText: 'Username',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none
                         ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none
+                        ),
+                        prefixIcon: Icon(Icons.person),
+                        hintText: 'UserName',
+                        border: InputBorder.none,
                         filled: true,
                         fillColor: Colors.grey[200],
                       ),
@@ -78,11 +93,17 @@ class MyApp extends StatelessWidget {
                     TextField(
                       obscureText: true,
                       decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        hintText: 'Password',
-                        border: OutlineInputBorder(
+                        enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none
                         ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none
+                        ),
+                        prefixIcon: Icon(Icons.lock),
+                        hintText: 'PassWord',
+                        border: InputBorder.none,
                         filled: true,
                         fillColor: Colors.grey[200],
                         suffixIcon: Icon(Icons.visibility),
@@ -98,15 +119,18 @@ class MyApp extends StatelessWidget {
                           'Reminder me next time',
                           style: TextStyle(fontSize: screenWidth * 0.04),
                         ),
-                        Switch(value: false, onChanged: (value) {}),
+                        Switch(value: false,
+                            activeColor: Color.fromARGB(255, 27, 55, 77),
+                            onChanged: (value) {}),
                       ],
                     ),
-                    SizedBox(height: screenHeight * 0.03),
+                    SizedBox(height: screenHeight * 0.01),
 
                     // Sign In button
                     ElevatedButton(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 27, 55, 77),
                         padding: EdgeInsets.symmetric(
                           vertical: screenHeight * 0.02,
                         ),
@@ -116,7 +140,10 @@ class MyApp extends StatelessWidget {
                       ),
                       child: Text(
                         'Sign In',
-                        style: TextStyle(fontSize: screenWidth * 0.045),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: screenWidth * 0.045,
+                        ),
                       ),
                     ),
                     SizedBox(height: screenHeight * 0.03),
@@ -134,8 +161,8 @@ class MyApp extends StatelessWidget {
                           child: Text(
                             'Sign Up',
                             style: TextStyle(
-                              color: Colors.blue,
-                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 27, 55, 77),
+                              fontWeight: FontWeight.w900,
                               fontSize: screenWidth * 0.04,
                             ),
                           ),
